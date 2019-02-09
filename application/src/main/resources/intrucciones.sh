b66a4c9d710ef446ca0b8cf0d9fab0f8e9a5f3e1
yobeto <beto.covarrubias01@gmail.com>
2019-02-09 01:40:26 -0600

    Subimos

---
+++ application/src/main/resources/intrucciones.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Intrucciones de comando
+
+wget http://samplecsvs.s3.amazonaws.com/Sacramentorealestatetransactions.csv
+wc -l Sacramentorealestatetransactions.csv
+head -10 | tail -10 Sacramentorealestatetransactions.csv
+grep "Multi-Family"  Sacramentorealestatetransactions.csv
+
